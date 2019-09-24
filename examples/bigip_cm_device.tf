3f30caf00c33ea6066e3e1dbd2c7277a386bbd48
RavinderReddyF5 <ravinder.nitt@gmail.com>
2019-09-24 10:18:34 +0000

    removed unwanted spaces

--- examples/bigip_cm_device.tf
+++ examples/bigip_cm_device.tf
@@ -3,15 +3,13 @@ Copyright 2019 F5 Networks Inc.
 This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0.
 If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
+
 provider "bigip" {
   address = "xxx.xxx.xxx.xxx" // bigip ip address //
   username = "xxxxx" 
   password = "xxxxx"
 }
-
-
 resource "bigip_cm_device" "my_new_device"
-
         {
             name = "bigip300.f5.com"
             configsync_ip = "2.2.2.2"
